0a01a969d814156e57d4955bccd43f1e47ce8e35
meixsosimp <linhmyx2003@gmail.com>
2024-01-14 03:54:28 +0700

    "one page"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'my_home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -16,65 +18,5 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff0b1d28),
-      appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.white, // Set color for the icon button
-        ),
-        backgroundColor: const Color(0xff074264),
-        leading: Image.asset(
-          "assets/logo_dart.png",
-        ),
-        title: const Text(
-          "Dart",
-          textAlign: TextAlign.start,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-      endDrawer: Drawer(
-        child: ListView(
-          children: const [
-            ListTile(
-              title: Text(
-                "Overview",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text(
-                "Community",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text(
-                "Docs",
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
